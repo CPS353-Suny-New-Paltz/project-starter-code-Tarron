64be0b924fc14a7667951f9b529e1e9f509844a0
Tarron Smith <Tarronsmithjr@gmail.com>
2025-08-27 18:30:41 -0400

    Update FailCheck.java
    Edited so FailCheck.java passes both checks.

--- src/project/annotations/FailCheck.java
+++ src/project/annotations/FailCheck.java
@@ -4,10 +4,11 @@ public class FailCheck {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This line will fail Gradle check because of missing semicolon")
+		//System.out.println("This line will fail Gradle check because of missing semicolon")
+		System.out.println("This should pass the Gradle check");
 		
-		System.out.println("This very very very very very very very very very very very very very very very very very very very very very very very very long line will fail the CheckStyle");
-
+		//System.out.println("This very... long line will fail the CheckStyle");
+		System.out.println("This short line will pass the CheckStyle check");
 	}
 
 }
