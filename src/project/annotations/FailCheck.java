b24befcd41d5fbdf975207fb76aac66142bc19cd
Tarron Smith <Tarronsmithjr@gmail.com>
2025-08-27 18:51:30 -0400

    Update FailCheck.java
    Reverted back to original passing code.

--- src/project/annotations/FailCheck.java
+++ src/project/annotations/FailCheck.java
@@ -4,11 +4,11 @@ public class FailCheck {
 
 	public static void main(String[] args) {
 		
-		//Will fail Gradle
-		System.out.println("This line will fail Gradle check because of missing semicolon")
+		//System.out.println("This line will fail Gradle check because of missing semicolon")
+		System.out.println("This should pass the Gradle check");
 		
-		//Will fail Checkstyle
-		System.out.println("This very very very very very very very very very very very very very very very very very very very very very very very very very very long line will fail the CheckStyle");
+		//System.out.println("This very very very very very very very very... long line will fail the CheckStyle");
+		System.out.println("This short line will pass the CheckStyle check");
 	}
 
 }
