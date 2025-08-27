c3a6dda8fee622959f8ea256335dfd95f5dca0fd
Tarron Smith <Tarronsmithjr@gmail.com>
2025-08-27 18:15:03 -0400

    Create FailCheck.java
    Add FailCheck.java to trigger Gradle and Checkstyle errors.

---
+++ src/project/annotations/FailCheck.java
@@ -0,0 +1,13 @@
+package project.annotations;
+
+public class FailCheck {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This line will fail Gradle check because of missing semicolon")
+		
+		System.out.println("This very very very very very very very very very very very very very very very very very very very very very very very very long line will fail the CheckStyle");
+
+	}
+
+}
