e79708fe766ee42baf349231ee76dc9a3b19136e
Tarron Smith <Tarronsmithjr@gmail.com>
2025-09-27 16:58:24 -0400

    feat(impl): added empty API implementations
    Created ComputeEngineImpl, StorageComputeImpl, and UserComputeImpl classes.
    Each class implements its respective API interface.
    Methods return default values (null) as placeholders.

---
+++ src/impl/ComputeEngineImpl.java
@@ -0,0 +1,19 @@
+package impl;
+
+import java.util.List;
+
+import api.compute.ComputeEngineAPI;
+
+/*
+ * Empty implementation, for now.
+ * Later, it will call the factorization algorithm.
+ */
+public class ComputeEngineImpl implements ComputeEngineAPI {
+
+	@Override
+	public List<Integer> factors(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/impl/UserComputeImpl.java
@@ -0,0 +1,15 @@
+package impl;
+
+import java.util.List;
+
+import api.user.UserComputeAPI;
+
+public class UserComputeImpl implements UserComputeAPI {
+
+	@Override
+	public List<Integer> computeFactors(String inputSource, String outputSource, String delimiter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
