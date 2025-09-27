b4c01abdc71580baeb971b287cc9811e931572a8
Tarron Smith <164373936+TarronSmith@users.noreply.github.com>
2025-09-27 17:05:33 -0400

    feat(checkpoint3): added empty API implementations (#38)
    * Update to build.gradle
    
    Updated build.gradle to exclude test placeholders, since no implementation exist currently.
    
    * Update to build.gradle (fix)
    
    changed  "excludeTestMatching" to "excludeTestsMatching"
    
    * feat(impl): added empty API implementations
    
    Created ComputeEngineImpl, StorageComputeImpl, and UserComputeImpl classes.
    Each class implements its respective API interface.
    Methods return default values (null) as placeholders.

---
+++ src/impl/StorageComputeImpl.java
@@ -0,0 +1,21 @@
+package impl;
+
+import java.util.List;
+
+import api.process.StorageComputeAPI;
+
+public class StorageComputeImpl implements StorageComputeAPI {
+
+	@Override
+	public List<Integer> readInput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeOutput(List<Integer> numbers, String delimiter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
