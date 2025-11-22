7df4a6b523a4707d7267dd5d312c3a25b189771f
Tarron Smith <Tarronsmithjr@gmail.com>
2025-11-21 19:02:06 -0500

    Fix
    -Fixed checkstyle failure

--- test/tests/TestComputeEngineValidation.java
+++ test/tests/TestComputeEngineValidation.java
@@ -4,7 +4,8 @@ import api.compute.ComputeEngineAPI;
 import impl.ComputeEngineImpl;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestComputeEngineValidation {
 	@Test
